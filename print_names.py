d7b22a7b89099f1b0275f6cbce4b589e0c6d603b
Azizullah Darmani <128895230+AzizullahDarmani@users.noreply.github.com>
2025-02-21 23:31:12 +0500

    Update print_names.py

--- print_names.py
+++ print_names.py
@@ -1,2 +1,9 @@
-print("Azizullah Darmani") 
+import sys
 
+if len(sys.argv) < 2:
+    print("Error: No names provided.")
+    sys.exit(1)
+
+print("Names from command-line arguments:")
+for name in sys.argv[1:]:
+    print(name)
